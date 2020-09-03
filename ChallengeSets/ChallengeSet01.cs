473cea249dcd8e68e1aff1ab5b1fae26ea59f982
Shilpi Thakur <shilpi.thakur@gmail.com>
2020-09-03 15:53:52 -0700

    Multiply Solved

--- ChallengeSets/ChallengeSet01.cs
+++ ChallengeSets/ChallengeSet01.cs
@@ -35,7 +35,7 @@ namespace ChallengeSets
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            return factor1 * factor2;
         }
 
         public string GetGreeting(string nameOfPerson)
